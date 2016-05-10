edbeeae58f5c297614620dd374e5682e3af78685
masabomber <iitsuka182@gmail.com>
2016-05-10 18:00:36 +0900

    no message

--- calculate-sales.rb
+++ calculate-sales.rb
@@ -3,31 +3,27 @@ require './branch.rb'
 require './commodity.rb'
 
 def main
-  return puts("支店定義ファイルが存在しません") if empty?("branch.lst")
+  return puts("支店定義ファイルが存在しません") if File.exist?("#{ARGV[0]}/branch.lst")
   unless branches = load("branch.lst", "^\\d{3}$", Branch)
     return puts("支店定義ファイルのフォーマットが不正です")
   end
-  return puts("商品定義ファイルが存在しません") if empty?("commodity.lst")
+  return puts("商品定義ファイルが存在しません") if File.exist?("#{ARGV[0]}/commodity.lst")
   unless commodities = load("commodity.lst", "^\\w{8}$", Commodity)
     return puts("商品定義ファイルのフォーマットが不正です")
   end
-  
+
   sales_files = Dir.glob "*.rcd"
   return puts("売上ファイル名が連番になっていません") if sales_files.size != sales_files.last.to_i
   sales_files.each do |file|
     sales_table = CSV.table("#{ARGV[0]}/#{file}", {headers: ["header"], converters: :date})
     return puts("#{file}のフォーマットが不正です") if sales_table.size != 3
     return puts("#{file}の支店コードが不正です") unless calculate!(sales_table[:header][0], sales_table, branches)
-    return puts("合計金額が10桁を超えました") if exceed_ten?(branches, sales_table[:header][0])
+    return puts("合計金額が10桁を超えました") if branches[sales_table[:header][0]].price.to_s.length < 10
     return puts("#{file}の商品コードが不正です") unless calculate!(sales_table[:header][1], sales_table, commodities)
-    return puts("合計金額が10桁を超えました") if exceed_ten?(commodities, sales_table[:header][1])
+    return puts("合計金額が10桁を超えました") if commodities[sales_table[:header][1]].price.to_s.length < 10
   end
   output("branch.out", branches)
   output("commodity.out", commodities)
-end
-
-def empty?(file_name)
-  not File.exist?("#{ARGV[0]}/#{file_name}")
 end
 
 def load(file_name, regex, klass)
@@ -43,10 +39,6 @@ end
 def calculate!(code, sales_table, hash)
   return nil if hash[code].nil?
   hash[code].price += sales_table[:header][2].to_i
-end
-
-def exceed_ten?(hash, code)
-  not hash[code].price.to_s.length < 10
 end
 
 def output(file_name, hash)
